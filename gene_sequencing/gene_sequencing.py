e46ab5e86258847fb654dd167b1116c80c7c0a74
michal.hryniewiecki <qa.m.hryniewiecki@gmail.com>
2024-04-23 07:07:38 +0200

    Refactor "Gene sequencing" algorithm

--- gene_sequencing/gene_sequencing.py
+++ gene_sequencing/gene_sequencing.py
@@ -1,4 +1,5 @@
 import pandas
+
 CODON_FILE_NAME = "codon.tsv"
 
 
@@ -9,9 +10,10 @@ class SequencingDNA:
         assert len(input) % 3 == 0, "The sample is not the right length"
         sample = [input[0 + i:3 + i] for i in range(0, len(input), 3)]
         df = pandas.read_csv(CODON_FILE_NAME, sep="\t", header=None)
-        codon_table_dict = df.to_dict(orient="records")
-        for code in sample:
-            for key in codon_table_dict:
-                if code == key[0]:
-                    dictionary[code] = key[1]
+        codon_list_dict = df.to_dict(orient="records")
+        dictionary = {key[0]: key[1] for key in codon_list_dict if key[0] in sample}
+        # for code in sample:
+        #     for key in codon_table_dict:
+        #         if code == key[0]:
+        #             dictionary[code] = key[1]
         return dictionary
